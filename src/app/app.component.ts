21ec0dab6e15df4ce31ae3814eb1ee3cdea0e883
Oleg <ignaten321@gmail.com>
2021-02-04 03:33:52 +0200

    implemented validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { RegisterModel } from './entities/registerModel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'OptiTech';
+
+export class AppComponent implements OnInit {
+  public registerModel: RegisterModel;
+  public isAllInputField: boolean;
+  public isOnblur = false;
+
+  @ViewChild('container1', { static: false }) container1: ElementRef;
+  @ViewChild('container2', { static: false }) container2: ElementRef;
+  @ViewChild('container3', { static: false }) container3: ElementRef;
+
+  click$: Observable<any> = fromEvent(document, 'click');
+  input$: Observable<any> = fromEvent(document, 'input');
+
+
+  private subscribeToObservables() {
+    this.click$.subscribe((x) => this.fillValueInForm(x.toElement.name));
+    this.input$.subscribe((x) => {
+      this.fillValueInForm(x.target.attributes.class.value,
+        (document.querySelector(`.${x.target.attributes.class.value}`) as HTMLTextAreaElement).value);
+    });
+  }
+
+
+  ngOnInit() {
+    this.registerModel = new RegisterModel();
+    this.subscribeToObservables();
+  }
+
+  private fillValueInForm(inputName: string, data?: string) {
+    if (inputName === 'email') { this.registerModel.email = data; }
+    if (inputName === 'userPassword') { this.registerModel.password = data; }
+    if (inputName === 'userPassword2') {
+
+      this.registerModel.password2 = data;
+      if (this.isOnblur) {
+        if (this.registerModel.password !== this.registerModel.password2) {
+          this.container3.nativeElement.innerHTML = `<p>password do not match</p`;
+        } else {
+          this.container3.nativeElement.innerHTML = ``;
+        }
+      }
+    }
+    if (inputName === 'register') { this.validationForm(); }
+    if (this.registerModel.password && this.registerModel.password2 && this.registerModel.password) {
+      this.isAllInputField = true;
+    } else {
+      this.isAllInputField = false;
+    }
+
+  }
+
+  public onblurValidation() {
+    if (this.registerModel.password !== this.registerModel.password2) {
+      this.container3.nativeElement.innerHTML = `<p>password do not match</p`;
+    } else {
+      this.container3.nativeElement.innerHTML = ``;
+    }
+  }
+
+  private validationForm() {
+    let isValid = true;
+
+    const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (reg.test(String(this.registerModel.email).toLowerCase()) === false) {
+      this.container1.nativeElement.innerHTML = `<p>wrong mail format</p`;
+      isValid = false;
+    } else {
+      this.container1.nativeElement.innerHTML = ``;
+    }
+
+    if (this.registerModel.password.length <= 4 && isValid) {
+      this.container2.nativeElement.innerHTML = `<p>too short password</p`;
+      isValid = false;
+    } else {
+      this.container2.nativeElement.innerHTML = ``;
+    }
+
+    if (this.registerModel.password !== this.registerModel.password2 && isValid) {
+      this.container3.nativeElement.innerHTML = `<p>password do not match</p`;
+      isValid = false;
+    } else {
+      this.container3.nativeElement.innerHTML = ``;
+    }
+
+    if (isValid) {
+      alert(`Email: ${this.registerModel.email}, Password: ${this.registerModel.password}`);
+    }
+
+  }
+
+
 }
---
+++ src/app/entities/registerModel.ts
@@ -0,0 +1,5 @@
+export class RegisterModel {
+   email: string;
+   password: string;
+   password2: string;
+}
